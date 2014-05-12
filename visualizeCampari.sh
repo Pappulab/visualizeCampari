fbf2410bcead7d29f9e9d2dfd821a815555a6557
ibharvey <burstolava@gmail.com>
2014-05-12 09:59:02 -0500

    Shell opens and closes sockets for OpenOffice and allows relative paths as input

--- visualizeCampari.sh
+++ visualizeCampari.sh
@@ -13,14 +13,21 @@
 ##	Pappu Lab
 ##	May 2014
 ##==============================================================================
+# Start up the OpenOffice port
+soffice --nologo --nodefault --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager"&
+
+# Get the absolute path to the directory
+ABSPATH="$(cd ${1%/*}; pwd)/${1##*/}"
 
 if [[ -d $1 ]]; then
 	# Graph all data desired
-	matlab -nosplash -nodesktop -r "graphCampari('$1');"
+	matlab -nosplash -nodesktop -r "graphCampari('$ABSPATH');"
 	# Generate a presentation of this data
-	python presentCampari.py -d $1
-	
+	python presentCampari.py -d $ABSPATH
 else
 	echo "$1 is not a directory."
 	exit 1
 fi
+
+# Close the OpenOffice port
+soffice --unaccept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager"&
